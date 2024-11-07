848cae1af5ef072e9adbfd1fb225407233e06a30
shivam upadhyay <shivamupadhyay2868@gmail.com>
2024-11-07 22:16:17 +0530

    enjoy it

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,8 @@ import searchRoutes from "./routes/search.route.js";
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
 import { protectRoute } from "./middleware/protectRoute.js";
-import { configDotenv } from "dotenv";
 
 const app = express();
-configDotenv();
 
 const PORT = ENV_VARS.PORT;
 const __dirname = path.resolve();
@@ -26,15 +24,15 @@ app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
 
-
+if (ENV_VARS.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
-
+}
 
 app.listen(PORT, () => {
-	console.log("Server started at https://vibe-nest.onrender.com/:" + PORT);
+	console.log("Server started at http://localhost:" + PORT);
 	connectDB();
 });
